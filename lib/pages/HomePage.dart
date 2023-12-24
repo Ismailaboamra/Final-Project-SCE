9920ee007f40e77893421baf014875d16be7b315
Ismailaboamra <ismail.amra12@gmail.com>
2023-12-24 17:58:29 +0200

    Login user student (frontend).

---
+++ lib/pages/HomePage.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page"),
+        titleTextStyle: TextStyle(fontSize: 27, color: Colors.black),
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(255, 0, 232, 159),
+      ),
+
+    );
+  }
+}
---
+++ lib/pages/LoginPage.dart
@@ -0,0 +1,120 @@
+// ignore_for_file: prefer_const_constructors, file_names
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../shared/MyTextField.dart';
+
+class LoginForm extends StatelessWidget {
+  const LoginForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: const Color.fromARGB(255, 248, 248, 248),
+        onPressed: () {
+          Navigator.pushNamed(context, '/LoginForm');
+        },
+        child: IconButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/LoginForm');
+            },
+            icon: Icon(
+              Icons.home,
+              color: Colors.black,
+            )),
+      ),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 0, 232, 159),
+        elevation: 15,
+        title: Text("LogIn Page"),
+        centerTitle: true,
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/LoginForm');
+            },
+            icon: Icon(
+              Icons.undo,
+              size: 33,
+            )),
+      ),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            SvgPicture.asset(
+              "assets/img/userlogo.svg",
+              alignment: Alignment.topCenter,
+              height: 120,
+              width: 150,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(40),
+                    color: const Color.fromARGB(255, 255, 255, 255)),
+                width: 300,
+                child: MyTextField(
+                    keyboardTypee: TextInputType.emailAddress,
+                    hintTextt: "USERNAME",
+                    obscureText: false,
+                    prefixIcon: Icons.person)),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(40),
+                    color: Colors.white),
+                width: 300,
+                child: MyTextField(
+                    keyboardTypee: TextInputType.visiblePassword,
+                    hintTextt: "Enter Your Password :",
+                    obscureText: true,
+                    prefixIcon: Icons.lock)),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all(Color.fromARGB(255, 0, 232, 159)),
+              ),
+              child: Text("LogIn",
+                  style: TextStyle(fontSize: 25, color: Colors.black)),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Don't have an account?",
+                  style: TextStyle(fontSize: 16),
+                ),
+                TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/SignUp');
+                    },
+                    child: Text(
+                      "SignUp",
+                      style: TextStyle(color: Colors.black, fontSize: 18),
+                    ))
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
