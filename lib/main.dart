d78335b941431049def9817aee1ad6ab84611754
Ismailaboamra <ismail.amra12@gmail.com>
2023-12-26 02:25:27 +0200

    setup firenase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 // ignore_for_file: unused_import, prefer_const_constructors, sort_child_properties_last
 
+import 'package:final_project_sce/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'pages/HomePage.dart';
 import 'pages/LoginPage.dart';
 import 'pages/SignUpPage.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+Future<void> main() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+     );
+    runApp(const MyApp());
+ }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
