d1f306e29ebe4f4358ddd588e62d89cd2d2fb887
BhaaAldenAldda <bhaawork21@gmail.com>
2024-02-20 18:51:16 +0200

    part Search for a mentor section in the mentors store in home page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -15,15 +15,56 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: SvgPicture.asset('assets/icons/sceMentor.svg', height: 24),
-      actions: [
-        IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.messenger_outline,
-            )),
-      ],
-    ));
+      appBar: AppBar(
+        title: SvgPicture.asset('assets/icons/sceMentor.svg', height: 24),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.messenger_outline,
+              )),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Stack(children: [
+              Container(
+                height: 300,
+                // width: 316,
+                margin: EdgeInsets.only(left: 0, right: 0),
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.fill,
+                        image: AssetImage('assets/img/background1.png'))),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  margin: EdgeInsets.only(left: 50, right: 50, top: 200),
+                  decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                      borderRadius: BorderRadius.circular(35)),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'What you want to learn?',
+                      prefixIcon: Icon(Icons.search),
+                    ),
+                  ),
+                ),
+              ),
+              // ),
+            ]),
+            SizedBox(
+              height: 40,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
